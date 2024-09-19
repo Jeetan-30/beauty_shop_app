37931eaccad1fb798de718fbf4de780b6afe6a6c
Jeetan <jeetankoydul13@gmail.com>
2024-09-19 11:47:30 +0600

    Initial commit

---
+++ lib/data/details_list.dart
@@ -0,0 +1,89 @@
+
+import 'package:beauty/model/details.dart';
+
+List<Detail1> products=[
+  Detail1(
+    image: "assets/products/image1.png",
+    name: "Gold Plan",
+    payment: "\$ 10",
+    brife: "Gold Plan is a strong and appealing name for a premium feature. It conveys a sense of value and exclusivity, which is perfect for a premium offering",
+    name2: 'Gold Plan',
+    quantity: 1,
+    rating: '',
+  ),
+  Detail1(
+    image: "assets/products/image3.png",
+    name: "Forever",
+    payment: "\$ 15",
+    brife: "While there’s no doubt that naturally pure less skin and no-makeup selfies will forever be the ultimate goal, sometimes you could use a little help enhancing your complexion.",
+    name2: 'Foundation',
+    quantity: 1,
+    rating: '',
+  ),
+  Detail1(
+    image: "assets/products/17_Best_Eyeshadow_Palettes-removebg-preview.png",
+    name: "Palettes",
+    payment: "\$ 18",
+    brife: "It contains various neutral, cool, and warm nude shades as well as some subtle pops of color that you can get creative with.",
+    name2: 'Eyeshadow',
+    quantity: 1,
+    rating: '',
+  ),
+  Detail1(
+    image: "assets/products/Concealer_1-removebg-preview.png",
+    name: "Blusher",
+    payment: "\$ 6",
+    brife: ". Comes in a beautiful Peach Bliss Naturally Sun kissed Pink shade that suits all skin tones, perfect for creating a fresh and youthful-looking complexion.",
+    name2: 'Blush',
+    quantity: 1,
+    rating: '',
+  ),
+  Detail1(
+    image: "assets/products/image4.png",
+    name: " Nailpolish",
+    payment: "\$ 13",
+    brife: "a premium, long-lasting lacquer designed for those who demand elegance and durability. This high-gloss formula glides on effortlessly, providing a smooth, even coat with a radiant finish that lasts for days.",
+    name2: 'Nailpolish',
+    quantity: 1,
+    rating: '',
+  ),
+];
+
+List<Detail2> lip=[
+  Detail2(
+      image: "assets/images/image6.png",
+      name: "Charlotte Tilbury ",
+      payment: "\$ 17",
+      brife: "brife",
+      quen: "200ml"
+  ),
+  Detail2(
+      image: "assets/images/lip4-removebg-preview.png",
+      name: "Faces Canada",
+      payment: "\$ 21",
+      brife: "brife",
+      quen: "200ml"
+  ),
+  Detail2(
+      image: "assets/images/lip3-removebg-preview.png",
+      name: "Huda Beauty",
+      payment: "\$ 20",
+      brife: "brife",
+      quen: "200ml"
+  ),
+  Detail2(
+      image: "assets/images/lip2-removebg-preview.png",
+      name: "M.A.C Matte Nude",
+      payment: "\$ 18",
+      brife: "brife",
+      quen: "200ml"
+  ),
+  Detail2(
+      image: "assets/images/lip5-removebg-preview.png",
+      name: "Lakmé Absolute",
+      payment: "\$ 19",
+      brife: "brife",
+      quen: "200ml"
+  ),
+
+];
